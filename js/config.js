7bb56689458a20ed771c104f61df86e546deaaa0
vadimgerij8-droid <vadimgerij8@gmail.com>
2026-06-27 10:14:39 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,22 @@
+export const PROXY_URL = 'https://monoanime.animegran8.workers.dev';
+export const ANIMEUA_BASE = 'https://animeua.club';
+export const GENRE_MAP = {
+    "Бойові мистецтва": "boivie",
+    "Бойовики": "boyovik",
+    "Воєнні": "voenne",
+    "Гарем": "garems",
+    "Драми": "drama",
+    "Детектив": "detektiv",
+    "Демони": "demons",
+    "Комедії": "komik",
+    "Роботи": "meha",
+    "Повсякденність": "posyardnevnist",
+    "Пригоди": "adventures",
+    "Психологічні": "psih",
+    "Романтика": "romantik",
+    "Надприродні": "weird",
+    "Фантастика": "fantastika",
+    "Фентезі": "fentezi",
+    "Школа": "classes",
+    "Еччі": "echhi"
+};
